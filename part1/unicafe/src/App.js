3174a1c3dc1e16180d8ff65c98c76e6855632457
Khanh Chung <khanh.d.chung@gmail.com>
2022-04-15 02:54:48 -0700

    1.9: unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,6 +5,8 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good - bad) / all
   const positive = good / all * 100
 
+  if (!all) return <div>No feedback given</div>
+
   return (
     <div>
       <div>good {good}</div>
