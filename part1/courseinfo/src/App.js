dca319e4b290eef568272662c2723aafa785800a
Khanh Chung <khanh.d.chung@gmail.com>
2022-04-16 23:16:29 -0700

    2.1: Course information step6

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,16 +1,14 @@
-const Header = (props) => (
-  <h1>{props.course}</h1>
+const Header = ({ course }) => (
+  <h1>{course}</h1>
 )
 
-const Part = (props) => (
-  <p>{props.part} {props.exercises}</p>
+const Part = ({ name, exercises }) => (
+  <p>{name} {exercises}</p>
 )
 
-const Content = (props) => (
+const Content = ({ parts }) => (
   <div>
-    <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-    <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-    <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+    {parts.map(part => <Part key={part.name} name={part.name} exercises={part.exercises} />)}
   </div>
 )
 
@@ -22,25 +20,7 @@ const Total = (props) => {
   )
 }
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
@@ -48,6 +28,32 @@ const App = () => {
       <Total parts={course.parts} />
     </div>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
