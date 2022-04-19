dabe1a7312d38f263a9def4bbcc9117437868f88
Khanh Chung <khanh.d.chung@gmail.com>
2022-04-19 01:10:49 -0700

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,17 +2,26 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [query, setQuery] = useState('')
+
+  const personsToShow = persons.filter(person => {
+    const name = person.name.toLowerCase()
+    return name.includes(query.toLowerCase())
+  })
 
   const handleSubmitClick = event => {
     event.preventDefault()
     
     const foundPerson = persons
       .find(person => person.name === newName)
-      
+
     if (foundPerson) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
@@ -33,9 +42,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleQueryChange = event => {
+    setQuery(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input 
+          value={query}
+          onChange={handleQueryChange}
+        />
+      </div>
       <form onSubmit={handleSubmitClick}>
         <div>
           name: 
@@ -57,7 +77,7 @@ const App = () => {
       </form>
       
       <h2>Numbers</h2>
-      {persons.map(person => (
+      {personsToShow.map(person => (
         <div key={person.name}>
           {person.name} {person.number}
         </div>
